dfbbbbee9e54405c6611b39d4080c5b04d1bd7b5
amitaima <amitai.malka@gmail.com>
2017-05-08 18:51:47 +0300

    im smarter than hallel and noam batiton - client

---
+++ final project - client/final project - client/Source.cpp
@@ -0,0 +1,29 @@
+
+#pragma comment (lib, "ws2_32.lib")
+
+#include <WinSock2.h>
+#include <Windows.h>
+#include "WSAInitializer.h"
+#include "Client.h"
+#include <exception>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	try
+	{
+		WSAInitializer wsaInit;
+		Client c1;
+		c1.Connect("127.0.0.1", 8876);
+		c1.startConversation();
+
+	}
+	catch (exception& e)
+	{
+		cout << "Error occured: " << e.what() << endl;
+	}
+	system("PAUSE");
+	return 0;
+}
