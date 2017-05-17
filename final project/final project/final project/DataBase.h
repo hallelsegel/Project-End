95479e5f1084b5b7b6e44dad74d4f0c58f4cd055
hallelbb <hallel.segel@gmail.com>
2017-05-17 22:10:22 +0300

    Hope now the project opens

---
+++ final project/final project/final project/DataBase.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include <list>
+#include <vector>
+#include <string>
+
+using namespace std;
+struct sqlite3;
+
+class DataBase
+{
+private:;
+	sqlite3* _sqldb;
+	int _lastId;
+
+	static int callbackCount(void* param, int argc, char** argv, char** azColName);
+	static int callbackQuestions(void* param, int argc, char** argv, char** azColName);
+	static int callbackBestScores(void* param, int argc, char** argv, char** azColName);
+	static int callbackPersonalStatus(void* param, int argc, char** argv, char** azColName);
+
+	bool initDatabase();
+
+public:
+	DataBase();
+	~DataBase();
+
+	bool isUserExists(string username);
+	bool addNewUser(string username, string password, string email);
+	bool isUserAndPassMatch(string password, string email);
+	vector<Question*> initQuestion(int questionNo);
+	int insertNewGame();
+	bool updateGameStatus(int gameId);
+	bool addAnswerToPlayer(int gameId, string username, int questionId, string answer, bool isCorrect, int answerTime);
+};
---
+++ final project/final project/final project/user.cpp
@@ -0,0 +1,89 @@
+#include "User.h"
+#include "Helper.h"
+#include "Server.h"
+
+User::User(string username, SOCKET sock)
+{
+	this->_username = username;
+	this->_sock = sock;
+}
+
+void User::send(string message)
+{
+	Helper::sendData(this->_sock, message);
+}
+
+void User::clearGame()
+{
+	this->_currGame = nullptr;
+}
+
+bool User::createRoom(int roomId, string roomName, int maxUsers, int questionsNo, int questionTime)
+{
+	if (this->_currRoom != nullptr)
+	{
+		send(this->_sock, "1141", 5, 0); //////*******************************************************//////
+		return false;
+	}
+	this->_currRoom = new Room(roomId, roomName, maxUsers, questionsNo, questionTime);
+	send(this->_sock, "1140", 5, 0); //////*******************************************************//////
+	return true;
+}
+
+bool User::joinRoom(Room*)
+{
+	if (this->_currRoom != nullptr)
+	{
+		return false;
+	}
+
+}
+
+void User::leaveRoom()
+{
+	if (this->_currRoom != nullptr) this->_currRoom = nullptr;
+}
+
+int User::closeRoom()
+{
+	if (this->_currRoom = nullptr) return -1;
+	int roomNum = this->_currRoom;
+	this->_currRoom = nullptr;
+	return roomNum;
+}
+
+bool User::leaveGame()
+{
+	if (this->_currGame = nullptr) return false;
+	
+	this->_currGame = nullptr;
+	return true;
+}
+
+// GETS
+string User::getUsername()
+{
+	return(this->_username);
+}
+
+SOCKET User::getSocket()
+{
+	return(this->_sock);
+}
+
+Room* User::getRoom()
+{
+	return(this->_currRoom);
+}
+
+Game* User::getGame()
+{
+	return(this->_currGame);
+}
+
+// SETS
+void User::setGame(Game* gm)
+{
+	this->_currRoom = NULL;
+	this->_currGame = gm;
+}
